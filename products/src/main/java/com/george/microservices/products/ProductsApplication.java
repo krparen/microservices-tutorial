7551b3c67dc9126579b74faa89ff25f2c2c822c3
krparen <ksacck@gmail.com>
2021-12-05 01:33:10 +0700

    products microservice registers in eureka discovery server

--- products/src/main/java/com/george/microservices/products/ProductsApplication.java
+++ products/src/main/java/com/george/microservices/products/ProductsApplication.java
@@ -2,7 +2,10 @@ package com.george.microservices.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductsApplication {
 
