ca8ed0e0a0b4c45f5f41917f5e7f5e3e7b64b146
krparen <ksacck@gmail.com>
2021-12-15 02:14:52 +0700

    ProductAggregate further developing

--- products/src/main/java/com/george/microservices/products/command/ProductAggregate.java
+++ products/src/main/java/com/george/microservices/products/command/ProductAggregate.java
@@ -1,14 +1,25 @@
 package com.george.microservices.products.command;
 
+import com.george.microservices.products.core.events.ProductCreatedEvent;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 
 @Aggregate
 @NoArgsConstructor
 public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
 
     @CommandHandler
     public ProductAggregate(CreateProductCommand createProductCommand) {
@@ -21,5 +32,20 @@ public class ProductAggregate {
                 || createProductCommand.getTitle().isBlank()) {
             throw new IllegalArgumentException("Price cannot be less or equal to zero");
         }
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+
+        AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.quantity = productCreatedEvent.getQuantity();
+        this.title = productCreatedEvent.getTitle();
+
     }
 }
