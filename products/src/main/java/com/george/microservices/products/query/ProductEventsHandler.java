0eab1165a085f7ab85e78d12a77a62e47eec5a6d
krparen <ksacck@gmail.com>
2021-12-26 18:57:33 +0700

    storing product event data in h2 file database

---
+++ products/src/main/java/com/george/microservices/products/query/ProductEventsHandler.java
@@ -0,0 +1,24 @@
+package com.george.microservices.products.query;
+
+import com.george.microservices.products.core.data.ProductEntity;
+import com.george.microservices.products.core.data.ProductsRepository;
+import com.george.microservices.products.core.events.ProductCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProductEventsHandler {
+
+    private final ProductsRepository productsRepository;
+
+    @EventHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        ProductEntity productEntity = new ProductEntity();
+        BeanUtils.copyProperties(productCreatedEvent, productEntity);
+
+        productsRepository.save(productEntity);
+    }
+}
