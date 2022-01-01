4b955a892d6217d40d69b0ce458e252184d00e66
krparen <ksacck@gmail.com>
2022-01-01 13:44:22 +0700

    product lookup entity and repo, registration of message interceptor

--- products/src/main/java/com/george/microservices/products/ProductsApplication.java
+++ products/src/main/java/com/george/microservices/products/ProductsApplication.java
@@ -1,9 +1,12 @@
 package com.george.microservices.products;
 
+import com.george.microservices.products.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -13,4 +16,9 @@ public class ProductsApplication {
         SpringApplication.run(ProductsApplication.class, args);
     }
 
+    @Autowired
+    public void registerCreatedProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+        CreateProductCommandInterceptor interceptor = context.getBean(CreateProductCommandInterceptor.class);
+        commandBus.registerDispatchInterceptor(interceptor);
+    }
 }
---
+++ products/src/main/java/com/george/microservices/products/core/data/ProductLookupRepository.java
@@ -0,0 +1,8 @@
+package com.george.microservices.products.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductLookupRepository extends JpaRepository<ProductLookupEntity, String> {
+
+    ProductLookupEntity findByProductIdOrTitle(String productId, String productTitle);
+}
