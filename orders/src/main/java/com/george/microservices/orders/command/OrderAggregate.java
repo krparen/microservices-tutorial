00a6a17ee95afd8768c43f369627f8618e57b241
krparen <ksacck@gmail.com>
2022-01-03 02:19:26 +0700

    OrderAggregate created and populated

---
+++ orders/src/main/java/com/george/microservices/orders/command/OrderAggregate.java
@@ -0,0 +1,41 @@
+package com.george.microservices.orders.command;
+
+import com.george.microservices.orders.core.events.OrderCreatedEvent;
+import lombok.Data;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.springframework.beans.BeanUtils;
+
+@Data
+public class OrderAggregate {
+
+    @AggregateIdentifier
+    private String orderId;
+    private String productId;
+    private String userId;
+    private int quantity;
+    private String addressId;
+    private OrderStatus orderStatus;
+
+    @CommandHandler
+    public OrderAggregate(CreateOrderCommand createOrderCommand) {
+        OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent();
+        BeanUtils.copyProperties(createOrderCommand, orderCreatedEvent);
+
+        AggregateLifecycle.apply(orderCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(OrderCreatedEvent orderCreatedEvent) {
+        this.orderId = orderCreatedEvent.getOrderId();
+        this.productId = orderCreatedEvent.getProductId();
+        this.userId = orderCreatedEvent.getUserId();
+        this.quantity = orderCreatedEvent.getQuantity();
+        this.addressId = orderCreatedEvent.getAddressId();
+        this.orderStatus = orderCreatedEvent.getOrderStatus();
+    }
+
+
+}
