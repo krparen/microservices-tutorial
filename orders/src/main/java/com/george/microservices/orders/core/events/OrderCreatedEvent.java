3deadee324b8615af4258275446a8787a6ecb122
krparen <ksacck@gmail.com>
2022-01-03 23:43:33 +0700

    new core module

--- orders/src/main/java/com/george/microservices/orders/core/events/OrderCreatedEvent.java
+++ orders/src/main/java/com/george/microservices/orders/core/events/OrderCreatedEvent.java
@@ -1,9 +1,13 @@
 package com.george.microservices.orders.core.events;
 
 import com.george.microservices.orders.command.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreatedEvent {
 
     private String orderId;
---
+++ orders/src/main/java/com/george/microservices/orders/saga/OrderSaga.java
@@ -0,0 +1,21 @@
+package com.george.microservices.orders.saga;
+
+import com.george.microservices.orders.core.events.OrderCreatedEvent;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Saga
+public class OrderSaga {
+
+    @Autowired
+    private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+
+    }
+}
